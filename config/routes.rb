1623f9a9676d7d724490ff30951be2d6b0e967c8
redmarverick <nyldrift@hotmail.com>
2023-09-23 04:42:39 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    devise_scope :user do
+      post 'login', to: 'sessions#create'
+      post 'register', to: 'registrations#create'
+      delete 'logout', to: 'sessions#destroy'
+      post 'password/forgot', to: 'passwords#forgot' # This route is for initiating password reset
+      put 'password/reset', to: 'passwords#reset'     # This route is for resetting the password
+    end
+  end
+
+  root 'root#index'
 end
