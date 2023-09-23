12ccddbbcb5a750b68f8bd6be8f20e119dc9d430
redmarverick <nyldrift@hotmail.com>
2023-09-23 04:41:58 -0300

    Create registrations_controller.rb

---
+++ app/controllers/api/registrations_controller.rb
@@ -0,0 +1,21 @@
+class Api::RegistrationsController < Devise::RegistrationsController
+  skip_before_action :verify_authenticity_token, only: :create
+  respond_to :json
+
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      sign_in(resource)
+      render json: { message: 'User created successfully.' }
+    else
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
