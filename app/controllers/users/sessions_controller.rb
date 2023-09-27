208b5d370140e0b7bfd7937ebe1e66d5c8f94bd1
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-09-27 16:15:52 +0500

    define function for login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,10 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   respond_to :json
   skip_before_action :verify_authenticity_token
 
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    sign_in(resource_name, resource)
-    render json: { message: 'Logged in successfully.' }
+  def respond_with(resource, _opts = {})
   end
 
   def destroy
