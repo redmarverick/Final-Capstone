a70db0eca7fc7ea84bb5687aa6d26261763ae0d6
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-09-27 16:17:22 +0500

    update login function

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,8 @@ class Users::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token
 
   def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: { status: 200, user: resource, message: 'Logged in Successfully' }
   end
 
   def destroy
