1339fcd21ef693263d77b3e33c02ee64ec7248e2
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-09-27 18:09:41 +0500

    update reservations

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -1,20 +1,2 @@
 class ReservationController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create]
-  before_action :authenticate_user!, only: [:index]
-  def index
-    @reservations = Reservation.find_by(user_id: params[:user_id])
-    render json: {cars_reservations: @reservations}
-  end
-  def create
-    @reserved = Reservation.new(reservation_params)
-    if @reserved.save
-      render json:{message: 'car reserved'}
-    else
-      render json:{error: @reserved.errors.full_messages}
-    end
-  end
-private
-  def reservation_params
-    params.require(:reservation).permit(:user_id, :car_id, :city, :date)
-  end
 end
