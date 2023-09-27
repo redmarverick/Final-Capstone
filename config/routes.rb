d0842a5dae201752a28908f2b00b28b6c4075c72
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-09-27 17:35:57 +0500

    add route for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'signup'
+  }, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
 
   root 'root#index'
