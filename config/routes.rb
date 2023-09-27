2edbe8452e89ae6beefa4f0008175a9d3bf8eeb3
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-09-27 17:35:10 +0500

    defining routes for app

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
+  # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
 
-  namespace :api do
-    devise_scope :user do
-      post 'login', to: 'sessions#create'
-      post 'register', to: 'registrations#create'
-      delete 'logout', to: 'sessions#destroy'
-      post 'password/forgot', to: 'passwords#forgot' # This route is for initiating password reset
-      put 'password/reset', to: 'passwords#reset'     # This route is for resetting the password
-    end
-    resources :cars, only: [:index, :show, :create, :update, :destroy] do
-      put 'update', on: :member
-    end
-  end
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  }
 
   root 'root#index'
 end
