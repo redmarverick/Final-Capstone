dc724fecfad6c27f4db18d7278485783f074c15c
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-09-27 16:59:34 +0500

    add index function in reservation

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -1,4 +1,8 @@
 class ReservationController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
   before_action :authenticate_user!, only: [:index]
+  def index
+    @reservations = Reservation.find_by(user_id: params[:user_id])
+    render json: {cars_reservations: @reservations}
+  end
 end
