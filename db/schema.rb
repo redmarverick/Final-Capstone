cd8c10aae284cd2bf7523eb7f5dd253d1c448a10
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-09-29 12:51:22 +0500

    add test for reservations

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_26_140732) do
     t.string "name"
     t.string "description"
     t.string "photo"
+    t.boolean "reserved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price"
