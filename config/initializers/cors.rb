e743b72ea723b20852b747551e7f306c3c0b1d28
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-10-05 03:57:25 +0500

    UPDATE

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://car-booking-app-piym.onrender.com/'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
